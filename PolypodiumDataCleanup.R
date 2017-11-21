ec3dd574d59b14bd945c8d39107d2df5c4806b96
jim161 <jonas.mendezreneau1@louisiana.edu>
2017-11-21 16:26:38 -0600

    added code to clean up and write table for P. virginianum and P. sibiricum

--- PolypodiumDataCleanup.R
+++ PolypodiumDataCleanup.R
@@ -1,6 +1,15 @@
 library (tidyr)
 
-#Remove samples with missing coordinates and write txt file for import into GIS
+#Remove samples with missing coordinates and write txt file for each species for import into GIS
 
+#P.appalachianum
 pappacleanup <- subset(Pappalachianum, !is.na(decimallatitude))
 write.table(pappacleanup, "Pappalachianum_coords.txt", sep="\t", row.names =FALSE, col.names=TRUE)
+
+#P.virgnianum
+virginiacleanup <- subset(Pvirginianum, !is.na(decimallatitude))
+write.table(virginiacleanup, "Pvirginianum_coords.txt", sep="\t", row.names =FALSE, col.names=TRUE)
+
+#P.sibiricum
+sibiricleanup <- subset(Psibiricum, !is.na(decimallatitude))
+write.table(sibiricleanup, "Psibiricum_coords.txt", sep="\t", row.names =FALSE, col.names=TRUE)
